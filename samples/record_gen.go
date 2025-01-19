fd1f40c2d33bd5762c952ee09c520c132290a093
Kazuma Arino <hogeika2@gmail.com>
2025-01-19 22:31:40 +0900

    Add samples.

---
+++ samples/record_gen.go
@@ -0,0 +1,10 @@
+package main
+
+type hoge struct {
+	X string
+	Y string
+}
+
+func ika() *hoge {
+	return &hoge{X: "abc", Y: "def"}
+}
---
+++ samples/two_func_gen.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func hello(msg string) {
+	fmt.Printf("Hello %s\n", msg)
+}
+
+func main() {
+	hello("World")
+}
