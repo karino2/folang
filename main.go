ccee585dd1936248ed7d8bc74559677b0eb0f950
Kazuma Arino <hogeika2@gmail.com>
2025-01-19 22:29:56 +0900

    Implement main.

--- main.go
+++ main.go
@@ -2,16 +2,36 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func main() {
+	files := os.Args[1:]
+	if len(files) == 0 {
+		fmt.Println("Usage: fc file1.fo file2.fo file3.fo ...")
+		return
+	}
+
 	tp := NewTranspiler()
-	f := NewFile(
-		[]Stmt{
-			&Package{"main"},
-			&Import{"fmt"},
-			&FuncDef{"main", nil, &GoEval{"fmt.Println(\"Hello World\")"}},
-		},
-	)
-	fmt.Println(tp.Transpile(f))
+	parser := &Parser{}
+	for _, file := range files {
+		buf, err := os.ReadFile(file)
+		check(err)
+		stmts := parser.Parse(file, buf)
+		f := NewFile(stmts)
+		res := tp.Transpile(f)
+
+		dir := filepath.Dir(file)
+		base := strings.TrimSuffix(filepath.Base(file), ".fo")
+		dest := filepath.Join(dir, base+"_gen.go")
+		os.WriteFile(dest, []byte(res), 0644)
+	}
 }
